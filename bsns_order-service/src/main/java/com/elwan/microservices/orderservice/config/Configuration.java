61dcb6aeded3e098afa56fa0a5cb91fe97c28164
tifa <mustafa.m.abdelsattar@gmail.com>
2020-05-01 08:14:58 +0200

    Apply some enhancements based on docker-compose module

--- bsns_order-service/src/main/java/com/elwan/microservices/orderservice/config/Configuration.java
+++ bsns_order-service/src/main/java/com/elwan/microservices/orderservice/config/Configuration.java
@@ -5,9 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
+import lombok.Data;
+
 @Component
 @ConfigurationProperties("")
 @RefreshScope
+@Data
 public class Configuration {
 
 	@Value("${app.title}")
@@ -19,27 +22,4 @@ public class Configuration {
 	@Value("${context.path.order}")
 	private String contextPath;
 
-	public String getAppTitle() {
-		return appTitle;
-	}
-
-	public void setAppTitle(String appName) {
-		this.appTitle = appName;
-	}
-
-	public int getRowsPerPage() {
-		return rowsPerPage;
-	}
-
-	public void setRowsPerPage(int rowsPerPage) {
-		this.rowsPerPage = rowsPerPage;
-	}
-
-	public String getContextPath() {
-		return contextPath;
-	}
-	
-	public void setContextPath(String contextPath) {
-		this.contextPath = contextPath;
-	}
 }
